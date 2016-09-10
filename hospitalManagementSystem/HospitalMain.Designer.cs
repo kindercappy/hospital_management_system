5dec4893635d02aa1dac6d7f00ea5a9b22704209
Kinderpreet <kp.cappy.kp@gmail.com>
2016-09-10 16:04:38 +0530

    Add HospitalMain and NewPatient designer.

---
+++ hospitalManagementSystem/HospitalMain.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace hospitalManagementSystem
+{
+    partial class HospitalMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNewPatient = new System.Windows.Forms.Label();
+            this.btnNewPatient = new System.Windows.Forms.Button();
+            this.lblExistingPatient = new System.Windows.Forms.Label();
+            this.btnExistingPatient = new System.Windows.Forms.Button();
+            this.lblPatient = new System.Windows.Forms.Label();
+            this.btnNewDoctor = new System.Windows.Forms.Button();
+            this.lblDoctor = new System.Windows.Forms.Label();
+            this.lblNewDoctor = new System.Windows.Forms.Label();
+            this.lblExistingDoctor = new System.Windows.Forms.Label();
+            this.btnExistingDoctor = new System.Windows.Forms.Button();
+            this.lblStaff = new System.Windows.Forms.Label();
+            this.lblNewStaff = new System.Windows.Forms.Label();
+            this.lblExistingStaff = new System.Windows.Forms.Label();
+            this.btnNewStaff = new System.Windows.Forms.Button();
+            this.btnExistingStaff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNewPatient
+            // 
+            this.lblNewPatient.AutoSize = true;
+            this.lblNewPatient.Location = new System.Drawing.Point(9, 25);
+            this.lblNewPatient.Name = "lblNewPatient";
+            this.lblNewPatient.Size = new System.Drawing.Size(65, 13);
+            this.lblNewPatient.TabIndex = 0;
+            this.lblNewPatient.Text = "New Patient";
+            // 
+            // btnNewPatient
+            // 
+            this.btnNewPatient.Location = new System.Drawing.Point(12, 41);
+            this.btnNewPatient.Name = "btnNewPatient";
+            this.btnNewPatient.Size = new System.Drawing.Size(122, 23);
+            this.btnNewPatient.TabIndex = 1;
+            this.btnNewPatient.Text = "New Patient";
+            this.btnNewPatient.UseVisualStyleBackColor = true;
+            // 
+            // lblExistingPatient
+            // 
+            this.lblExistingPatient.AutoSize = true;
+            this.lblExistingPatient.Location = new System.Drawing.Point(193, 25);
+            this.lblExistingPatient.Name = "lblExistingPatient";
+            this.lblExistingPatient.Size = new System.Drawing.Size(79, 13);
+            this.lblExistingPatient.TabIndex = 2;
+            this.lblExistingPatient.Text = "Existing Patient";
+            // 
+            // btnExistingPatient
+            // 
+            this.btnExistingPatient.Location = new System.Drawing.Point(150, 41);
+            this.btnExistingPatient.Name = "btnExistingPatient";
+            this.btnExistingPatient.Size = new System.Drawing.Size(122, 23);
+            this.btnExistingPatient.TabIndex = 3;
+            this.btnExistingPatient.Text = "Existing Patient";
+            this.btnExistingPatient.UseVisualStyleBackColor = true;
+            // 
+            // lblPatient
+            // 
+            this.lblPatient.AutoSize = true;
+            this.lblPatient.Location = new System.Drawing.Point(120, 9);
+            this.lblPatient.Name = "lblPatient";
+            this.lblPatient.Size = new System.Drawing.Size(40, 13);
+            this.lblPatient.TabIndex = 4;
+            this.lblPatient.Text = "Patient";
+            // 
+            // btnNewDoctor
+            // 
+            this.btnNewDoctor.Location = new System.Drawing.Point(12, 99);
+            this.btnNewDoctor.Name = "btnNewDoctor";
+            this.btnNewDoctor.Size = new System.Drawing.Size(122, 23);
+            this.btnNewDoctor.TabIndex = 5;
+            this.btnNewDoctor.Text = "New Doctor";
+            this.btnNewDoctor.UseVisualStyleBackColor = true;
+            // 
+            // lblDoctor
+            // 
+            this.lblDoctor.AutoSize = true;
+            this.lblDoctor.Location = new System.Drawing.Point(120, 67);
+            this.lblDoctor.Name = "lblDoctor";
+            this.lblDoctor.Size = new System.Drawing.Size(39, 13);
+            this.lblDoctor.TabIndex = 6;
+            this.lblDoctor.Text = "Doctor";
+            // 
+            // lblNewDoctor
+            // 
+            this.lblNewDoctor.AutoSize = true;
+            this.lblNewDoctor.Location = new System.Drawing.Point(9, 83);
+            this.lblNewDoctor.Name = "lblNewDoctor";
+            this.lblNewDoctor.Size = new System.Drawing.Size(64, 13);
+            this.lblNewDoctor.TabIndex = 7;
+            this.lblNewDoctor.Text = "New Doctor";
+            // 
+            // lblExistingDoctor
+            // 
+            this.lblExistingDoctor.AutoSize = true;
+            this.lblExistingDoctor.Location = new System.Drawing.Point(194, 83);
+            this.lblExistingDoctor.Name = "lblExistingDoctor";
+            this.lblExistingDoctor.Size = new System.Drawing.Size(78, 13);
+            this.lblExistingDoctor.TabIndex = 8;
+            this.lblExistingDoctor.Text = "Existing Doctor";
+            // 
+            // btnExistingDoctor
+            // 
+            this.btnExistingDoctor.Location = new System.Drawing.Point(150, 99);
+            this.btnExistingDoctor.Name = "btnExistingDoctor";
+            this.btnExistingDoctor.Size = new System.Drawing.Size(122, 23);
+            this.btnExistingDoctor.TabIndex = 9;
+            this.btnExistingDoctor.Text = "Existing Doctor";
+            this.btnExistingDoctor.UseVisualStyleBackColor = true;
+            // 
+            // lblStaff
+            // 
+            this.lblStaff.AutoSize = true;
+            this.lblStaff.Location = new System.Drawing.Point(128, 125);
+            this.lblStaff.Name = "lblStaff";
+            this.lblStaff.Size = new System.Drawing.Size(29, 13);
+            this.lblStaff.TabIndex = 10;
+            this.lblStaff.Text = "Staff";
+            // 
+            // lblNewStaff
+            // 
+            this.lblNewStaff.AutoSize = true;
+            this.lblNewStaff.Location = new System.Drawing.Point(9, 142);
+            this.lblNewStaff.Name = "lblNewStaff";
+            this.lblNewStaff.Size = new System.Drawing.Size(54, 13);
+            this.lblNewStaff.TabIndex = 11;
+            this.lblNewStaff.Text = "New Staff";
+            // 
+            // lblExistingStaff
+            // 
+            this.lblExistingStaff.AutoSize = true;
+            this.lblExistingStaff.Location = new System.Drawing.Point(204, 142);
+            this.lblExistingStaff.Name = "lblExistingStaff";
+            this.lblExistingStaff.Size = new System.Drawing.Size(68, 13);
+            this.lblExistingStaff.TabIndex = 12;
+            this.lblExistingStaff.Text = "Existing Staff";
+            // 
+            // btnNewStaff
+            // 
+            this.btnNewStaff.Location = new System.Drawing.Point(12, 158);
+            this.btnNewStaff.Name = "btnNewStaff";
+            this.btnNewStaff.Size = new System.Drawing.Size(122, 23);
+            this.btnNewStaff.TabIndex = 13;
+            this.btnNewStaff.Text = "New Staff";
+            this.btnNewStaff.UseVisualStyleBackColor = true;
+            // 
+            // btnExistingStaff
+            // 
+            this.btnExistingStaff.Location = new System.Drawing.Point(150, 158);
+            this.btnExistingStaff.Name = "btnExistingStaff";
+            this.btnExistingStaff.Size = new System.Drawing.Size(122, 23);
+            this.btnExistingStaff.TabIndex = 14;
+            this.btnExistingStaff.Text = "Existing Staff";
+            this.btnExistingStaff.UseVisualStyleBackColor = true;
+            // 
+            // HospitalMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExistingStaff);
+            this.Controls.Add(this.btnNewStaff);
+            this.Controls.Add(this.lblExistingStaff);
+            this.Controls.Add(this.lblNewStaff);
+            this.Controls.Add(this.lblStaff);
+            this.Controls.Add(this.btnExistingDoctor);
+            this.Controls.Add(this.lblExistingDoctor);
+            this.Controls.Add(this.lblNewDoctor);
+            this.Controls.Add(this.lblDoctor);
+            this.Controls.Add(this.btnNewDoctor);
+            this.Controls.Add(this.lblPatient);
+            this.Controls.Add(this.btnExistingPatient);
+            this.Controls.Add(this.lblExistingPatient);
+            this.Controls.Add(this.btnNewPatient);
+            this.Controls.Add(this.lblNewPatient);
+            this.Name = "HospitalMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNewPatient;
+        private System.Windows.Forms.Button btnNewPatient;
+        private System.Windows.Forms.Label lblExistingPatient;
+        private System.Windows.Forms.Button btnExistingPatient;
+        private System.Windows.Forms.Label lblPatient;
+        private System.Windows.Forms.Button btnNewDoctor;
+        private System.Windows.Forms.Label lblDoctor;
+        private System.Windows.Forms.Label lblNewDoctor;
+        private System.Windows.Forms.Label lblExistingDoctor;
+        private System.Windows.Forms.Button btnExistingDoctor;
+        private System.Windows.Forms.Label lblStaff;
+        private System.Windows.Forms.Label lblNewStaff;
+        private System.Windows.Forms.Label lblExistingStaff;
+        private System.Windows.Forms.Button btnNewStaff;
+        private System.Windows.Forms.Button btnExistingStaff;
+    }
+}
+
---
+++ hospitalManagementSystem/NewPatient.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace hospitalManagementSystem
+{
+    partial class NewPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtBxFirstName = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.txtbxLastName = new System.Windows.Forms.TextBox();
+            this.comboBxDepartment = new System.Windows.Forms.ComboBox();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtBxAge = new System.Windows.Forms.TextBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.comboBxSex = new System.Windows.Forms.ComboBox();
+            this.lblHeightFt = new System.Windows.Forms.Label();
+            this.txtBxHeightFt = new System.Windows.Forms.TextBox();
+            this.lblHeightInch = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.txtBxWeight = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtBxPhone = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBxEmail = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtBxAddress = new System.Windows.Forms.TextBox();
+            this.lblNationality = new System.Windows.Forms.Label();
+            this.comboBxNationality = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(12, 9);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // txtBxFirstName
+            // 
+            this.txtBxFirstName.Location = new System.Drawing.Point(112, 6);
+            this.txtBxFirstName.Name = "txtBxFirstName";
+            this.txtBxFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtBxFirstName.TabIndex = 1;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(12, 39);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(61, 13);
+            this.lblLastName.TabIndex = 2;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // txtbxLastName
+            // 
+            this.txtbxLastName.Location = new System.Drawing.Point(112, 36);
+            this.txtbxLastName.Name = "txtbxLastName";
+            this.txtbxLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtbxLastName.TabIndex = 3;
+            // 
+            // comboBxDepartment
+            // 
+            this.comboBxDepartment.FormattingEnabled = true;
+            this.comboBxDepartment.Location = new System.Drawing.Point(112, 63);
+            this.comboBxDepartment.Name = "comboBxDepartment";
+            this.comboBxDepartment.Size = new System.Drawing.Size(121, 21);
+            this.comboBxDepartment.TabIndex = 4;
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(12, 66);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(65, 13);
+            this.lblDepartment.TabIndex = 5;
+            this.lblDepartment.Text = "Department:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 92);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 6;
+            this.lblAge.Text = "Age:";
+            // 
+            // txtBxAge
+            // 
+            this.txtBxAge.Location = new System.Drawing.Point(112, 89);
+            this.txtBxAge.Name = "txtBxAge";
+            this.txtBxAge.Size = new System.Drawing.Size(100, 20);
+            this.txtBxAge.TabIndex = 7;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(12, 119);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(28, 13);
+            this.lblSex.TabIndex = 8;
+            this.lblSex.Text = "Sex:";
+            // 
+            // comboBxSex
+            // 
+            this.comboBxSex.FormattingEnabled = true;
+            this.comboBxSex.Location = new System.Drawing.Point(112, 116);
+            this.comboBxSex.Name = "comboBxSex";
+            this.comboBxSex.Size = new System.Drawing.Size(121, 21);
+            this.comboBxSex.TabIndex = 9;
+            // 
+            // lblHeightFt
+            // 
+            this.lblHeightFt.AutoSize = true;
+            this.lblHeightFt.Location = new System.Drawing.Point(12, 145);
+            this.lblHeightFt.Name = "lblHeightFt";
+            this.lblHeightFt.Size = new System.Drawing.Size(80, 13);
+            this.lblHeightFt.TabIndex = 10;
+            this.lblHeightFt.Text = "Height (In feet):";
+            // 
+            // txtBxHeightFt
+            // 
+            this.txtBxHeightFt.Location = new System.Drawing.Point(112, 142);
+            this.txtBxHeightFt.Name = "txtBxHeightFt";
+            this.txtBxHeightFt.Size = new System.Drawing.Size(100, 20);
+            this.txtBxHeightFt.TabIndex = 11;
+            // 
+            // lblHeightInch
+            // 
+            this.lblHeightInch.AutoSize = true;
+            this.lblHeightInch.Location = new System.Drawing.Point(12, 171);
+            this.lblHeightInch.Name = "lblHeightInch";
+            this.lblHeightInch.Size = new System.Drawing.Size(94, 13);
+            this.lblHeightInch.TabIndex = 12;
+            this.lblHeightInch.Text = "Height (In Inches):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(12, 201);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(44, 13);
+            this.lblWeight.TabIndex = 14;
+            this.lblWeight.Text = "Weight:";
+            // 
+            // txtBxWeight
+            // 
+            this.txtBxWeight.Location = new System.Drawing.Point(112, 198);
+            this.txtBxWeight.Name = "txtBxWeight";
+            this.txtBxWeight.Size = new System.Drawing.Size(100, 20);
+            this.txtBxWeight.TabIndex = 15;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(12, 227);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(41, 13);
+            this.lblPhone.TabIndex = 16;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // txtBxPhone
+            // 
+            this.txtBxPhone.Location = new System.Drawing.Point(112, 224);
+            this.txtBxPhone.Name = "txtBxPhone";
+            this.txtBxPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtBxPhone.TabIndex = 17;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(12, 253);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 18;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtBxEmail
+            // 
+            this.txtBxEmail.Location = new System.Drawing.Point(112, 250);
+            this.txtBxEmail.Name = "txtBxEmail";
+            this.txtBxEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtBxEmail.TabIndex = 19;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(12, 306);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(48, 13);
+            this.lblAddress.TabIndex = 20;
+            this.lblAddress.Text = "Address:";
+            // 
+            // txtBxAddress
+            // 
+            this.txtBxAddress.Location = new System.Drawing.Point(112, 303);
+            this.txtBxAddress.Multiline = true;
+            this.txtBxAddress.Name = "txtBxAddress";
+            this.txtBxAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtBxAddress.TabIndex = 21;
+            // 
+            // lblNationality
+            // 
+            this.lblNationality.AutoSize = true;
+            this.lblNationality.Location = new System.Drawing.Point(12, 279);
+            this.lblNationality.Name = "lblNationality";
+            this.lblNationality.Size = new System.Drawing.Size(56, 13);
+            this.lblNationality.TabIndex = 23;
+            this.lblNationality.Text = "Nationality";
+            // 
+            // comboBxNationality
+            // 
+            this.comboBxNationality.FormattingEnabled = true;
+            this.comboBxNationality.Location = new System.Drawing.Point(112, 276);
+            this.comboBxNationality.Name = "comboBxNationality";
+            this.comboBxNationality.Size = new System.Drawing.Size(121, 21);
+            this.comboBxNationality.TabIndex = 24;
+            // 
+            // NewPatient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 504);
+            this.Controls.Add(this.comboBxNationality);
+            this.Controls.Add(this.lblNationality);
+            this.Controls.Add(this.txtBxAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.txtBxEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtBxPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtBxWeight);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblHeightInch);
+            this.Controls.Add(this.txtBxHeightFt);
+            this.Controls.Add(this.lblHeightFt);
+            this.Controls.Add(this.comboBxSex);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.txtBxAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.comboBxDepartment);
+            this.Controls.Add(this.txtbxLastName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.txtBxFirstName);
+            this.Controls.Add(this.lblFirstName);
+            this.Name = "NewPatient";
+            this.Text = "NewPatient";
+            this.Load += new System.EventHandler(this.NewPatient_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtBxFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.TextBox txtbxLastName;
+        private System.Windows.Forms.ComboBox comboBxDepartment;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtBxAge;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.ComboBox comboBxSex;
+        private System.Windows.Forms.Label lblHeightFt;
+        private System.Windows.Forms.TextBox txtBxHeightFt;
+        private System.Windows.Forms.Label lblHeightInch;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.TextBox txtBxWeight;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtBxPhone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBxEmail;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtBxAddress;
+        private System.Windows.Forms.Label lblNationality;
+        private System.Windows.Forms.ComboBox comboBxNationality;
+    }
+}
